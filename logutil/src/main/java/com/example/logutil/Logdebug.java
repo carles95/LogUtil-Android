a1dd4fd06b7132865699daaa08ad83d6a4e8eb78
carles95 <carlesadipianto@gmail.com>
2018-07-17 15:38:05 +0700

    create project  in studio. create to Logdebug for easy debugging

---
+++ logutil/src/main/java/com/example/logutil/Logdebug.java
@@ -0,0 +1,23 @@
+package com.example.logutil;
+
+import android.util.Log;
+
+/**
+ * Created by carles adp on 17/07/2018.
+ */
+
+public class Logdebug {
+      private static final String TAG = "SUPER_AWESOME_APP";
+   public static void d(String message){
+
+
+       Log.d(TAG, message);
+
+   }
+
+
+
+}
+
+
+
